e7f2a24c495f30c5b346dcb9ad9a207aa36abc70
MazzGuille <mazzcode@gmail.com>
2022-05-31 17:00:19 -0300

    Agregar archivos de proyecto.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+
+namespace WFPComboBox
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            List<Capitales> ListaCapitales = new List<Capitales>();
+
+            ListaCapitales.Add(new Capitales { NombreCapital = "Buenos Aires," + " ", Pais = "Argetina" });
+            ListaCapitales.Add(new Capitales { NombreCapital = "Caracas," + " ", Pais = "Venezuela" });
+            ListaCapitales.Add(new Capitales { NombreCapital = "Bogota," + " ", Pais = "Colombia" });
+            ListaCapitales.Add(new Capitales { NombreCapital = "Sao Paulo," + " ", Pais = "Brasil" });
+            ListaCapitales.Add(new Capitales { NombreCapital = "Madrid," + " ", Pais = "España" });
+
+            Capitales.ItemsSource = ListaCapitales;
+        }
+
+    }
+
+    public class Capitales
+    {
+        public string NombreCapital { get; set; }
+        public string Pais { get; set; }
+
+    }
+}
